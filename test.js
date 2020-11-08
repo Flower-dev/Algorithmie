bb0fa10f355e8e8ad820b68720c6e785072fd73d
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2020-11-08 16:14:09 +0100

    exo 2 fin du problème d'algo

--- test.js
+++ test.js
@@ -41,30 +41,54 @@ function parseToRoman(number) {
   numberOfThousand = parseInt(number/1000)
   moduloNumber = number % 1000
 
+  numberOfNineHundred = parseInt(moduloNumber/900)
+  moduloNumber = moduloNumber % 900
+
   numberOfFiveHundred = parseInt(moduloNumber/500)
   moduloNumber = moduloNumber % 500
+
+  numberOfFourHundred = parseInt(moduloNumber/400)
+  moduloNumber = moduloNumber % 400
 
   numberOfHundred = parseInt(moduloNumber/100)
   moduloNumber = moduloNumber % 100
 
+  numberOfNinety = parseInt(moduloNumber/90)
+  moduloNumber = moduloNumber % 90
+
   numberOfFifty = parseInt(moduloNumber/50)
   moduloNumber = moduloNumber % 50
+
+  numberOfFourty = parseInt(moduloNumber/40)
+  moduloNumber = moduloNumber % 40
 
   numberOfTen = parseInt(moduloNumber/10)
   moduloNumber = moduloNumber % 10
 
+  numberOfNine = parseInt(moduloNumber/9)
+  moduloNumber = moduloNumber % 9
+
   numberOfFive = parseInt(moduloNumber/5)
   moduloNumber = moduloNumber % 5
+
+  numberOfFour = parseInt(moduloNumber/4)
+  moduloNumber = moduloNumber % 4
 
   numberOfOne = parseInt(moduloNumber/1)
   moduloNumber = moduloNumber % 1
 
   var output = "M".repeat(numberOfThousand)
+  output += "CM" .repeat(numberOfNineHundred)
   output += "D".repeat(numberOfFiveHundred)
+  output += "CD" .repeat(numberOfFourHundred)
   output += "C".repeat(numberOfHundred)
+  output += "XC" .repeat(numberOfNinety)
   output += "L".repeat(numberOfFifty)
+  output += "XL" .repeat(numberOfFourty)
   output += "X".repeat(numberOfTen)
+  output += "IX" .repeat(numberOfNine)
   output += "V".repeat(numberOfFive)
+  output += "IV" .repeat(numberOfFour)
   output += "I".repeat(numberOfOne)
 
   console.log(output)
