d50b01d738e7fe0fe930e36a0ea2d852ea54896d
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2020-11-10 16:38:46 +0100

    exo 3 rot13

--- test.js
+++ test.js
@@ -98,6 +98,26 @@ function parseToRoman(number) {
 CONSIGNE :
 
 On va faire notre petite machine de Turing et on va craquer un algorithme de chiffrement. Nous allons donc essayer de craquer ce qu’on appelle le ROT13 (ou Rotate By 13 Places). L’idée est de décaler chacune des lettres de l’alphabet de 13 places. Autrement dit, le H devient le U, le E devient le R, etc.
+
+Ajouter 13 à chaque lettre
+
+A <=> N
+B <=> O
+C <=> P
+D <=> Q
+E <=> R
+F <=> S
+G <=> T
+H <=> U
+I <=> V
+J <=> W
+K <=> X
+L <=> Y
+M <=> Z
+
+Pseudo code : faire une fonction dans le but de déchiffrer chacune des lettres du mot ou de la phrase
+faire cela lettre par lettre
+faire lettre + 13 
 **/
 
 
