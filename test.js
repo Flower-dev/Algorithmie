05cd4445bceb15083740c6c2cc2be3eded566127
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2020-11-12 15:51:54 +0100

    exos hexadecimal

--- test.js
+++ test.js
@@ -28,6 +28,26 @@ console.log(sumOfNumber); /*pour afficher le résultat*/
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 /*
+CONSIGNE : 
+convertir les nombres hexadécimaux de la liste N en nombres binaires et additionner tous les nombres pairs entre eux
+*/
+
+var moduloNumber = 0;
+var sumOfNumber = 0;
+
+const N = parseInt(readline());
+for (let i = 0; i < N; i++) {
+  const S = readline();
+  let numberDecimal = parseInt(S,16);
+  moduloNumber = numberDecimal % 2;
+  if (moduloNumber == 0){
+    sumOfNumber = sumOfNumber + numberDecimal
+  }
+}
+console.log(sumOfNumber)
+
+
+/*
 CONSIGNE :
 Créer une fonction appelé parseToRoman qui, pour un nombre donné, va retourner sa valeur en chiffres romains.
 
