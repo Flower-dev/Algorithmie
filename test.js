253f073c9efeeab6f61dc348448a1ac1ced1e3d7
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2020-11-14 18:41:44 +0100

    new exercice

--- test.js
+++ test.js
@@ -223,3 +223,47 @@ var myNumber = 15;
 if (myNumber > 10){
 	 alert('le nombre est supérieur à 10');
 }
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/*CONSIGNE
+
+Ecrivez la condition suivante sour forme de condition ternaire.
+
+var result, number = 42;
+			
+if (number > 10) {
+    result = 'Plus grand que 10';
+} else {
+    result = 'Plus petit que 10';
+}
+*/
+
+var result, number = 42;
+result = (number > 10) ? 'Plus grand que 10' : 'Plus petit que 10';
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/*CONSIGNE
+
+Afin de jouer avec les opérateurs, et notamment l'opérateur modulo, voici un exercice assez classique : 
+déterminer si une année est bissextile.
+Pour rappel, une année est bissextile si elle est divisible par 4 sans être divisible par 100 ou si elle est divisible par 400
+Pour second rappel, l'opérateur modulo (%) permet d'obtenir le reste d'une division. Si le reste vaut 0, 
+c'est que la division est complète : if (8 % 2) { alert("8 est divisible par 2"): }.
+*/
+
+var year = 2016;
+
+/*On teste d'abord si l'année est divisible par 4 avec year % 4 === 0 tout en s'assurant que ce n'est pas divisible par 100, 
+avec year % 100 > 0. Et avec ||, on regarde si ce n'est quand même pas divisible par 400*/
+
+if ((year % 4 === 0 && year % 100 > 0) || (year % 400 === 0)){
+	console.log(year + "est bissextile");
+} else {
+	console.log(year + "n'est pas bissextile")
+	}
+
+var year = 2003
+
+if ((year % 4 === 0 && year % 100 > 0))
